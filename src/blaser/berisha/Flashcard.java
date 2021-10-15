2e34ffa00944f916b15085827227c61f8cca4346
blerineberisha <blerineberisha@gmail.com>
2021-10-15 10:59:16 +0200

    basic structure: classes Flashcard, QuizManager and Set added

---
+++ src/blaser/berisha/Flashcard.java
@@ -0,0 +1,37 @@
+package blaser.berisha;
+
+public class Flashcard {
+    private String word;
+    private String definition;
+    private int correct;
+
+    public Flashcard(String word, String definition, int correct) {
+        this.word = word;
+        this.definition = definition;
+        this.correct = correct;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+}
---
+++ src/blaser/berisha/Set.java
@@ -0,0 +1,49 @@
+package blaser.berisha;
+
+import java.util.ArrayList;
+
+public class Set {
+    private ArrayList<Flashcard> set;
+    private String title;
+    private String zipCode;
+    private String country;
+
+    public Set(ArrayList<Flashcard> set, String title, String zipCode, String country) {
+        this.set = set;
+        this.title = title;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
+
+    public ArrayList<Flashcard> getSet() {
+        return set;
+    }
+
+    public void setSet(ArrayList<Flashcard> set) {
+        this.set = set;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
